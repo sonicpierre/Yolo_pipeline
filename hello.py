4922d76cd80ce129c07abe4d5c359939798600f4
sonicpierre <pvirgaux@gmail.com>
2022-07-02 18:22:16 +0200

    Multiple Worspace

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import os
 print("Hello World !!")
 
-print(os.listdir("inputs/"))
+print(os.listdir("inputs/demo-dataset/"))
 """
 with open('data/salut_mon_gas.txt', 'r') as f:
     print(f.readlines())
