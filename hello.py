7001ae9ab1ca62664fa5981b987690f58758a38d
sonicpierre <pvirgaux@gmail.com>
2022-07-02 19:24:02 +0200

    Multiple Worspace

--- hello.py
+++ hello.py
@@ -4,7 +4,5 @@ print("Hello World !!")
 with open('inputs/demo-dataset/salut_mon_gas.txt', 'r') as f:
     line = f.readlines()[0]
 
-with open('outputs/ma-couille/output.txt', 'w') as f:
+with open('outputs/my-volume/output.txt', 'w') as f:
     f.write(line + "YOUPI")
-
-print(os.listdir('outputs/ma-couille'))
