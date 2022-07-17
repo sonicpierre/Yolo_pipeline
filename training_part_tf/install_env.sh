fc42aca8d5d782785d17ec3e2ec36a32488afba4
sonicpierre <pvirgaux@gmail.com>
2022-07-17 12:20:28 +0200

    Début TF model

--- training_part_tf/install_env.sh
+++ training_part_tf/install_env.sh
@@ -3,6 +3,6 @@ curl -OL https://github.com/google/protobuf/releases/download/v3.15.8/$PROTOC_ZI
 unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 unzip -o $PROTOC_ZIP -d /usr/local include/*
 rm -f $PROTOC_ZIP
-protoc /inputs/repo_models/models/research/object_detection/protos/*.proto --python_out=. && cp /inputs/repo_models/models/research/object_detection/packages/tf2/setup.py . && python -m pip install .
-python /inputs/repo_models/models/research/object_detection/builders/model_builder_tf2_test.py
+protoc /inputs/repomodels/models/research/object_detection/protos/*.proto --python_out=. && cp /inputs/repomodels/models/research/object_detection/packages/tf2/setup.py . && python -m pip install .
+python /inputs/repomodels/models/research/object_detection/builders/model_builder_tf2_test.py
 pip install tensorflow --upgrade
