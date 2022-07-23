d43b271530fb3aca4c5af815bdad871d984af80d
Pierre Virgaux <pierre.virgaux@eisti.fr>
2022-07-23 23:36:47 +0200

    Youtube implementation

--- training_part_tf/install_env.sh
+++ training_part_tf/install_env.sh
@@ -4,6 +4,4 @@ unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 unzip -o $PROTOC_ZIP -d /usr/local include/*
 rm -f $PROTOC_ZIP
 cd /inputs/repomodels/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install .
-pip uninstall -y tensorflow
-pip install tensorflow==2.5.0
 cd
