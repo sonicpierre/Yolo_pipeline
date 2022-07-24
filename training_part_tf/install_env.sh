26d5293bc25ebe527ef5d2a97ae42795507caeaa
sonicpierre <pvirgaux@gmail.com>
2022-07-24 15:40:48 +0200

    Pipeline debug

--- training_part_tf/install_env.sh
+++ training_part_tf/install_env.sh
@@ -1,8 +1,6 @@
-PROTOC_ZIP=protoc-3.15.8-linux-x86_64.zip
-curl -OL https://github.com/google/protobuf/releases/download/v3.15.8/$PROTOC_ZIP
-unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-unzip -o $PROTOC_ZIP -d /usr/local include/*
-rm -f $PROTOC_ZIP
-cd /inputs/repomodels/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install .
-pip install tensorflow --upgrade
+apt-get update && yes | apt-get upgrade
+apt-get install -y git python3-pip
+apt-get install ffmpeg libsm6 libxext6 -y
+DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get install -y protobuf-compiler python-pil python-lxml
+cd /inputs/repomodels/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python3 -m pip install .
 cd
