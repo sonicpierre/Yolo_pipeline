55331427fb9e5ab0215163879c4ad199f69275b6
sonicpierre <pvirgaux@gmail.com>
2022-07-24 19:03:53 +0200

    Working on class

--- training_part_tf/config_pipeline.py
+++ training_part_tf/config_pipeline.py
@@ -13,7 +13,7 @@ with tf.io.gfile.GFile("/inputs/mymodel/ssd_mobilenet_v2_fpnlite_320x320_coco17_
     text_format.Merge(proto_str, pipeline_config)
 
 pipeline_config.model.ssd.num_classes = len(LABELS)
-pipeline_config.train_config.batch_size = 16
+pipeline_config.train_config.batch_size = 8
 pipeline_config.train_config.fine_tune_checkpoint = os.path.join("/inputs/mymodel", "ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8", 'checkpoint', 'ckpt-0')
 pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
 pipeline_config.train_input_reader.label_map_path= "/inputs/tfrecordsmythumb/labelmap.pbtxt"
